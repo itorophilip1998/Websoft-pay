1db18d58f3dbb44c19c8a3bed7451f321cf0f7ff
itorophilip1998 <itorophilip1998@gmail.com>
2021-05-14 09:28:41 +0100

    updated

---
+++ database/factories/LogsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Logs;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LogsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Logs::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
